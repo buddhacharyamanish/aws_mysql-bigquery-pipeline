3a4629c858ac486e7b1792172970cd495d736bf5
Manish Ratna Buddhacharya <buddhacharya.manish@gmail.com>
2026-03-11 13:53:23 +1100

    Update generate dummy to insert dummy data into Sample table 2

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -17,10 +17,18 @@ for _ in range(1000):
 
     name = fake.name()
     created_date = fake.date_time()
+    email = fake.safe_email()
+    phoneno = fake.numerify("04########")
+
+
+    #cursor.execute(
+    #    "INSERT INTO sample_table (name, created_date) VALUES (%s,%s)",
+    #    (name, created_date)
+    #)
 
     cursor.execute(
-        "INSERT INTO sample_table (name, created_date) VALUES (%s,%s)",
-        (name, created_date)
+        "INSERT INTO sample_table2 (email, phoneno) VALUES (%s,%s)",
+        (email, phoneno)
     )
 
 conn.commit()
